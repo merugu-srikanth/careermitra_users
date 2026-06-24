7ace7eeb3384f4a2b212983b45f3aa7f3b117289
merugu-srikanth <merugusrikanth28@gmail.com>
2026-06-24 13:50:54 +0530

    Sitemap added env

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-8d0d8005'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.i233bo3n5k4"
+    "revision": "0.a22q08mpb7"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
