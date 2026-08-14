bba503a30cc47c4f123b57b54b280c2274a1afb7
merugu-srikanth <merugusrikanth28@gmail.com>
2026-08-14 12:57:07 +0530

    successfully updated the Fofigue file redirections

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* Config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.careermitra.in',
+          },
+        ],
+        destination: 'https://careermitra.in/:path*',
+        permanent: true, // 301
+      },
+    ];
+  },
 };
 
 export default nextConfig;
